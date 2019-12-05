f5a9c2ac2c6b185cf97ba1e8a89d6d5b30e0e70f
nigelbbc <nigel.millward@bbc.co.uk>
2019-12-05 09:48:32 +0000

    Remove pyscache and update gitignore

--- src/main/files/files_cli.py
+++ src/main/files/files_cli.py
@@ -7,7 +7,7 @@ def files():
 
 
 @files.command(help='Delete files from your downloads directory')
-@click.option('--days', default=30, show_default=True, help='Delete files > days')
+@click.option('--days', default=7, show_default=True, help='Delete files > days')
 def delete_downloads(days):
     from files.files import delete_downloads
     delete_downloads(days)
